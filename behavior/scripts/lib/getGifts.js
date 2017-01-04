bc0d9955b551de74f3a4e956956695f00108a5c8
Vince Lwt <vincent.loewert@gmail.com>
2017-01-04 22:09:08 +0100

    Update conversation

--- behavior/scripts/lib/getGifts.js
+++ behavior/scripts/lib/getGifts.js
@@ -2,7 +2,8 @@
 
 const request = require('request')
 
-module.exports = function getGifts(genre, age, budget, next) {
+module.exports = function getGiftsAmazon(genre, age, budget, next) {
+  console.log("Getting gift list for "+boy+" "+age+"yo with"+budget);
   //const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${weatherAPIKey}&q=${locationName}`
 
   //console.log('Making HTTP GET request to:', requestUrl)
