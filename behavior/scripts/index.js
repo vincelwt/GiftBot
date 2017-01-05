567832c0c6b45e4788280f9db5f4acfd878ddf9e
Vince Lwt <vincent.loewert@gmail.com>
2017-01-05 19:56:25 +0100

    Add some test data

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -76,72 +76,15 @@ exports.handle = function handle(client) {
     prompt(callback) {
       console.log("Almost done!");
       const environment = client.getCurrentApplicationEnvironment()
-      //getGiftsAmazon(client.getConversationState().genre.value, client.getConversationState().age.value, client.getConversationState().budget.value, giftsData => {
-        /*if (!resultBody || resultBody.cod !== 200) {
-          console.log('Error getting weather.')
-          callback()
-          return
-        }
+      getGiftsAmazon(client.getConversationState().genre.value, client.getConversationState().age.value, client.getConversationState().budget.value, giftsData => {
 
-        const weatherDescription = (
-          resultBody.weather.length > 0 ?
-          resultBody.weather[0].description :
-          null
-        )
-
-        const weatherData = {
-          temperature: Math.round(resultBody.main.temp),
-          condition: weatherDescription,
-          city: resultBody.name,
-        }
-
-        console.log('sending real weather:', weatherData)*/
-        const giftsData = {
-          links: 'http://amzn.eu/fqUJPBH'
-        }
-
-        client.addResponse('provide_gifts', giftsData)
         client.addCarouselListResponse({
-          items: [
-            {
-              media_url: 'https://c2.staticflickr.com/4/3512/5763418254_e2f42b2224_b.jpg',
-              media_type: 'image/jpeg',
-              description: 'Yosemite is a really nice place.',
-              title: 'Yosemite',
-              actions: [
-                {
-                  type: 'postback',
-                  text: 'Visit',
-                  payload: {
-                    data: {
-                      action: 'visit',
-                      park: 'yosemite'
-                    },
-                    version: '1',
-                    stream: 'selectPark',
-                  },
-                },
-              ],
-            },
-            {
-              media_url: 'https://upload.wikimedia.org/wikipedia/commons/3/36/Morning_Glory_Pool.jpg',
-              media_type: 'image/jpeg',
-              description: 'Yellowstone showcases geology in its most raw form.',
-              title: 'Yellowstone',
-              actions: [
-                {
-                  type: 'link',
-                  text: 'View info',
-                  uri: 'https://en.wikipedia.org/wiki/Yellowstone_National_Park',
-                },
-              ],
-            },
-          ],
+          items: giftsData,
         })
         client.done()
 
-      //  callback()
-      //})
+        callback()
+      })
     },
   })
 
--- behavior/scripts/lib/getGifts.js
+++ behavior/scripts/lib/getGifts.js
@@ -4,6 +4,34 @@ const request = require('request')
 
 module.exports = function getGiftsAmazon(genre, age, budget, next) {
   console.log("Getting gift list for "+boy+" "+age+"yo with"+budget);
+  
+  var giftsdata = require('./gifts.json');
+
+  var selected = treated = [];
+
+
+  for (g of giftsdata) {
+    if ((age >= g[5] || g[5] == '') && (age <= g[6] || g[5] == '') && (g[7] == genre.toLowerCase()) && (g[4] < budget) ) {
+      selected.push(g);
+    }
+  }
+
+  for (g of selected) {
+    treated.push({
+        media_url: g[3],
+        media_type: 'image/jpeg',
+        description: g[2],
+        title: g[1],
+        actions: [
+          {
+            type: 'link',
+            text: 'View',
+            uri: g[8],
+          },
+        ],
+    })
+  }
+
   //const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${weatherAPIKey}&q=${locationName}`
 
   //console.log('Making HTTP GET request to:', requestUrl)
@@ -23,6 +51,5 @@ module.exports = function getGiftsAmazon(genre, age, budget, next) {
 
   })*/
 
-  const parsedResult = "http://amzn.eu/0E9blr9"
-  next(parsedResult);
+  next(treated);
 }
