52d918997986224a055a8950272d9bace524970d
Vince Lwt <vincent.loewert@gmail.com>
2017-01-05 20:08:10 +0100

    Fix script

--- behavior/scripts/lib/getGifts.js
+++ behavior/scripts/lib/getGifts.js
@@ -7,7 +7,8 @@ module.exports = function getGiftsAmazon(genre, age, budget, next) {
   
   var giftsdata = require('./gifts.json');
 
-  var selected = treated = [];
+  var selected = [];
+  var treated = [];
 
 
   for (g of giftsdata) {
@@ -31,25 +32,6 @@ module.exports = function getGiftsAmazon(genre, age, budget, next) {
         ],
     })
   }
-
-  //const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${weatherAPIKey}&q=${locationName}`
-
-  //console.log('Making HTTP GET request to:', requestUrl)
-
-  /*request(requestUrl, (err, res, body) => {
-    if (err) {
-      throw new Error(err)
-    }
-
-    if (body) {
-      const parsedResult = JSON.parse(body)
-      next(parsedResult)
-    } else {
-      next()
-    }
-
-
-  })*/
 
   next(treated);
 }
